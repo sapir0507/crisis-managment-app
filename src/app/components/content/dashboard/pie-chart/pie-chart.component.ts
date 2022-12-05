55cabc1c25c98d4b04988c7524cb4e84412d3dbf
sapir0507 <sapir.s@info.cts.org.il>
2022-12-05 03:24:00 +0200

    pie chart connected to db

--- src/app/components/content/dashboard/pie-chart/pie-chart.component.ts
+++ src/app/components/content/dashboard/pie-chart/pie-chart.component.ts
@@ -12,6 +12,14 @@ Exporting(Highcharts);
 ExportData(Highcharts);
 Accessibility(Highcharts);
 
+export interface DataItem {
+  name: string,
+  y: number,
+  sliced?: boolean,
+  selected?: boolean
+  color?: string
+}
+
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -26,23 +34,14 @@ export class PieChartComponent {
     private crisisService: UtilsService
     ){}
 
-    
-
   async ngOnInit(){
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.   
    
     this.DB = await this.crisisService.get_all_crisis()
-    console.log(this.DB);
-
-    var crisis_counter:number, low_counter:number, medium_counter:number;
-
-    low_counter = ELEMENT_DATA.filter((item)=>{return item.severity===1}).length
-    medium_counter = ELEMENT_DATA.filter((item)=>{return item.severity===2}).length
-    crisis_counter = ELEMENT_DATA.filter((item)=>{return item.severity===3}).length
     
-    this.dataItems[0].y = crisis_counter;
-    this.dataItems[1].y = medium_counter;
-    this.dataItems[2].y = low_counter;
+    this.dataItems[0].y = this.DB.filter((item)=>{return item.severity===1}).length;
+    this.dataItems[1].y = this.DB.filter((item)=>{return item.severity===2}).length;
+    this.dataItems[2].y = this.DB.filter((item)=>{return item.severity===3}).length;
     
     Highcharts.chart('container', this.options);
   }
@@ -115,122 +114,6 @@ export class PieChartComponent {
   }
 }
 
-export interface DataItem {
-  name: string,
-  y: number,
-  sliced?: boolean,
-  selected?: boolean
-  color?: string
-}
 
 
-export interface PeriodicElement {
-  id: number;
-  _id?: number;
-  createdDate: string;
-  severity: number;
-  status: number;
-}
 
-
-const ELEMENT_DATA: PeriodicElement[] = [{
-  id:1,
-  createdDate:'01/09/22',
-  severity:1,
-  status:0
-  },{
-  id:2,
-  createdDate:'22/09/22',
-  severity:1,
-  status:0
-  },{
-  id:3,
-  createdDate:'01/09/22',
-  severity:2,
-  status:1
-  },{
-  id:4,
-  createdDate:'03/09/22',
-  severity:3,
-  status:0
-  },{
-  id:5,
-  createdDate:'03/09/22',
-  severity:2,
-  status:1
-  },{
-  id:6,
-  createdDate:'10/09/22',
-  severity:3,
-  status:1
-  },{
-  id:7,
-  createdDate:'01/09/22',
-  severity:3,
-  status:1
-  },{
-  id:8,
-  createdDate:'02/09/22',
-  severity:1,
-  status:1
-  },{
-  id:9,
-  createdDate:'07/09/22',
-  severity:1,
-  status:1
-  },{
-  id:10,
-  createdDate:'08/09/22',
-  severity:1,
-  status:0
-  },{
-  id:11,
-  createdDate:'05/09/22',
-  severity:3,
-  status:1
-  },{
-  id:12,
-  createdDate:'10/09/22',
-  severity:1,
-  status:0
-  },{
-  id:13,
-  createdDate:'01/09/22',
-  severity:3,
-  status:0
-  },{
-  id:14,
-  createdDate:'04/09/22',
-  severity:3,
-  status:1
-  },{
-  id:15,
-  createdDate:'04/09/22',
-  severity:1,
-  status:0
-  },{
-  id:16,
-  createdDate:'05/09/22',
-  severity:2,
-  status:0
-  },{
-  id:17,
-  createdDate:'03/09/22',
-  severity:1,
-  status:1
-  },{
-  id:18,
-  createdDate:'03/09/22',
-  severity:2,
-  status:0
-  },{
-  id:19,
-  createdDate:'10/09/22',
-  severity:3,
-  status:0
-  },{
-  id:20,
-  createdDate:'01/09/22',
-  severity:3,
-  status:0
-}]
