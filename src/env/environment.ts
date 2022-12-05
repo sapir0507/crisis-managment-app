60933ea64414ad4341a32f080c1e8eb9a2f671f8
sapir0507 <sapir.s@info.cts.org.il>
2022-12-05 04:40:28 +0200

    rm unused from env

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,17 +1,4 @@
 export const environment = {
     api: "http://localhost:3001/",
     production: false,
-    
-    firebase: {
-        
-    }
 }
-
-// rules_version = '2';
-// service cloud.firestore {
-//   match /databases/{database}/documents {
-//     match /{document=**} {
-//       allow read, write: if false;
-//     }
-//   }
-// }
